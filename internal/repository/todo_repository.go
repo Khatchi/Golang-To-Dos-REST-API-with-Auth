2a4363acc2587ab3cf692afcc395a68c38120c10
Khatchi <Solomon9ice2014@gmail.com>
2026-04-02 12:39:08 +0100

    feat: DeleteTodo repository method and the corresponding handler implemented...

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"time"
 	"todo_api/internal/models"
 
@@ -145,4 +146,26 @@ func UpdateTodo(pool *pgxpool.Pool, id int, title string, completed bool) (*mode
 	}
 
 	return &todo, nil
+}
+
+func DeleteTodo(pool *pgxpool.Pool, id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `
+		DELETE FROM todos
+		WHERE id = $1
+	`
+
+	commandTag, err := pool.Exec(ctx, query, id)
+
+	if err != nil {
+		return err
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		return fmt.Errorf("todo with id %d not found", id)
+	}
+
+	return nil
 }
